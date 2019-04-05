f23f8abc0cadcf1b73bd6d7eda8d5e00b02d9cbd
simon <sdiaz.sz@gmail.com>
2019-04-05 07:24:58 -0300

    add csv file read

--- src/Migration/MigrationSeeder.php
+++ src/Migration/MigrationSeeder.php
@@ -10,10 +10,35 @@ namespace Larangular\MigrationPackage\Migration;
 
 trait MigrationSeeder {
 
-    public function getData($filepath){
-        if(file_exists($filepath)) {
-            return json_decode(file_get_contents($filepath), true);
+    public function getData(string $filepath): array {
+        $response = [];
+        if (file_exists($filepath)) {
+            switch ($this->migrationSeeder_getExtension($filepath)) {
+                case 'csv':
+                    $response = $this->migrationSeeder_LoadCsv($filepath);
+                    break;
+                default:
+                    $response = json_decode(file_get_contents($filepath), true);
+                    break;
+            }
         }
+        return $response;
     }
+
+
+    private function migrationSeeder_getExtension(string $filepath): string {
+        return array_pop(explode('.', $filepath));
+    }
+
+    private function migrationSeeder_LoadCsv(string $filepath): array {
+        $rows = array_map('str_getcsv', file($filepath));
+        $header = array_shift($rows);
+        $csv = [];
+        foreach ($rows as $row) {
+            $csv[] = array_combine($header, $row);
+        }
+        return $csv;
+    }
+
 
 }
