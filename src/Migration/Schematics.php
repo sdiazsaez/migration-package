bb03c995b25a7b0aa7dc13444ad76d5d358f5c75
simon <sdiaz.sz@gmail.com>
2020-11-11 13:21:31 -0300

    [New] add jsonableColumnType

--- src/Migration/Schematics.php
+++ src/Migration/Schematics.php
@@ -1,30 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: simon
- * Date: 3/25/18
- * Time: 05:31
- */
 
 namespace Larangular\MigrationPackage\Migration;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 trait Schematics {
 
-    public function create(\Closure $callback){
-
+    public function create(\Closure $callback): void {
         Schema::connection($this->connection)
             ->create($this->name, function(Blueprint $table) use ($callback)
             {
                 $callback($table);
             });
-
     }
 
-    public function drop(){
+    public function drop(): void {
         Schema::connection($this->connection)->drop($this->name);
+    }
+
+    public function jsonableColumnType(): string {
+        $driverName = DB::connection($this->connection)
+                        ->getPdo()
+                        ->getAttribute(PDO::ATTR_DRIVER_NAME);
+        $dbVersion = DB::connection($this->connection)
+                       ->getPdo()
+                       ->getAttribute(PDO::ATTR_SERVER_VERSION);
+        $isOldVersion = version_compare($dbVersion, '5.7.8', 'lt');
+
+        return $driverName === 'mysql' && $isOldVersion
+            ? 'text'
+            : 'json';
     }
 
 }
